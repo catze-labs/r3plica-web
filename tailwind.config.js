9a964cd73911afc16ce90d057feaf537def26d83
aiiiden <hjjam100@gmail.com>
2022-12-17 14:36:00 +0900

    Initialize Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const { colors } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +10,40 @@ module.exports = {
     "./features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        black: {
+          dark: "#0C0E12",
+          DEFAULT: "#1E2328",
+        },
+        gray: {
+          DEFAULT: "#A3A3A3",
+          light: "EAECEF",
+          dark: "#292E33",
+        },
+        yellow: "#F2D74C",
+        orange: "#FF7914",
+        brown: "#C6A150",
+      },
+      fontFamily: {
+        sans: ["Rosario", ...fontFamily.sans],
+      },
+      keyframes: {
+        "buoyancy-1": {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-8px)" },
+        },
+        "buoyancy-2": {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-16px)" },
+        },
+      },
+      animation: {
+        "buoyancy-1": "buoyancy-1 2s ease-in-out infinite",
+        "buoyancy-2": "buoyancy-2 2s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
