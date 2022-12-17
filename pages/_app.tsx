98a5da1eddb1b6dfff11ea23d344c7b6ef457f41
aiiiden <hjjam100@gmail.com>
2022-12-17 14:35:05 +0900

    Initialize ToastContainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import "@fontsource/dm-sans";
+import "typeface-rosario";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +24,7 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <RecoilRoot>
         <QueryClientProvider client={queryClient}>
+          <ToastContainer />
           <Component {...pageProps} />
         </QueryClientProvider>
       </RecoilRoot>
